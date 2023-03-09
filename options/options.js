029aec9570f2888fa1525f6ed754b22dd6664d3c
Crowdbotics <team@crowdbotics.com>
2023-03-09 18:27:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ewallet-39305",
+  url: "https://ewallet-39305.botics.co",
+  api: "https://ewallet-39305.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
