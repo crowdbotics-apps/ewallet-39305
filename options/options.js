c587753ef5f140bec3ae2a39c6a7b58d52903cb7
Md.Al Amin <al.amin@crowdbotics.com>
2023-03-11 20:25:01 +0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "ewallet-39305",
-  url: "https://ewallet-39305.botics.co",
-  api: "https://ewallet-39305.botics.co/api/v1"
-}
+  "name": "ewallet-39305",
+  "url": "https://ewallet-39305.botics.co",
+  "api": "https://ewallet-39305.botics.co/api/v1",
+  "initialRoute": "Onboarding"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "copy": "Routes available!"
   }
-}
+};
