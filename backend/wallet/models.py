48d2f62b9a95ec757fad2d3eb7c2243bea438113
crowdbotics-apps <team@crowdbotics.com>
2023-05-09 09:25:46 +0000

    Update models

--- backend/wallet/models.py
+++ backend/wallet/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Wallet(models.Model):
     'Generated Model'
     wallet_id = models.BigIntegerField()
-    wallet_name = models.CharField(max_length=256,null=True,blank=True,)
-    wallet_type = models.CharField(max_length=256,null=True,blank=True,)
-    chain_support = models.CharField(max_length=256,null=True,blank=True,)
+    wallet_name = models.CharField(null=True,blank=True,max_length=256,)
+    wallet_type = models.CharField(null=True,blank=True,max_length=256,)
+    chain_support = models.CharField(null=True,blank=True,max_length=256,)
 
 # Create your models here.
