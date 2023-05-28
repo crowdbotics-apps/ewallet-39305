b5d51c9a9fefb15793baea128ad07e1294bf55bc
Crowdbotics <engineering@crowdbotics.com>
2023-05-28 19:18:47 +0000

    Update store.

--- store/ewalletAPI/mybalances.slice.js
+++ store/ewalletAPI/mybalances.slice.js
@@ -1,93 +1,93 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import { apiService } from "./api"
-export const api_v1_wallet_list = createAsyncThunk(
-  "wallets/api_v1_wallet_list",
+export const api_v1_mybalance_list = createAsyncThunk(
+  "mybalances/api_v1_mybalance_list",
   async payload => {
-    const response = await apiService.api_v1_wallet_list(payload)
+    const response = await apiService.api_v1_mybalance_list(payload)
     return response.data
   }
 )
-export const api_v1_wallet_create = createAsyncThunk(
-  "wallets/api_v1_wallet_create",
+export const api_v1_mybalance_create = createAsyncThunk(
+  "mybalances/api_v1_mybalance_create",
   async payload => {
-    const response = await apiService.api_v1_wallet_create(payload)
+    const response = await apiService.api_v1_mybalance_create(payload)
     return response.data
   }
 )
-export const api_v1_wallet_retrieve = createAsyncThunk(
-  "wallets/api_v1_wallet_retrieve",
+export const api_v1_mybalance_retrieve = createAsyncThunk(
+  "mybalances/api_v1_mybalance_retrieve",
   async payload => {
-    const response = await apiService.api_v1_wallet_retrieve(payload)
+    const response = await apiService.api_v1_mybalance_retrieve(payload)
     return response.data
   }
 )
-export const api_v1_wallet_update = createAsyncThunk(
-  "wallets/api_v1_wallet_update",
+export const api_v1_mybalance_update = createAsyncThunk(
+  "mybalances/api_v1_mybalance_update",
   async payload => {
-    const response = await apiService.api_v1_wallet_update(payload)
+    const response = await apiService.api_v1_mybalance_update(payload)
     return response.data
   }
 )
-export const api_v1_wallet_partial_update = createAsyncThunk(
-  "wallets/api_v1_wallet_partial_update",
+export const api_v1_mybalance_partial_update = createAsyncThunk(
+  "mybalances/api_v1_mybalance_partial_update",
   async payload => {
-    const response = await apiService.api_v1_wallet_partial_update(payload)
+    const response = await apiService.api_v1_mybalance_partial_update(payload)
     return response.data
   }
 )
-export const api_v1_wallet_destroy = createAsyncThunk(
-  "wallets/api_v1_wallet_destroy",
+export const api_v1_mybalance_destroy = createAsyncThunk(
+  "mybalances/api_v1_mybalance_destroy",
   async payload => {
-    const response = await apiService.api_v1_wallet_destroy(payload)
+    const response = await apiService.api_v1_mybalance_destroy(payload)
     return response.data
   }
 )
 const initialState = { entities: [], api: { loading: "idle", error: null } }
-const walletsSlice = createSlice({
-  name: "wallets",
+const mybalancesSlice = createSlice({
+  name: "mybalances",
   initialState,
   reducers: {},
   extraReducers: {
-    [api_v1_wallet_list.pending]: (state, action) => {
+    [api_v1_mybalance_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [api_v1_wallet_list.fulfilled]: (state, action) => {
+    [api_v1_mybalance_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = action.payload
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_list.rejected]: (state, action) => {
+    [api_v1_mybalance_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_create.pending]: (state, action) => {
+    [api_v1_mybalance_create.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [api_v1_wallet_create.fulfilled]: (state, action) => {
+    [api_v1_mybalance_create.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities.push(action.payload)
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_create.rejected]: (state, action) => {
+    [api_v1_mybalance_create.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_retrieve.pending]: (state, action) => {
+    [api_v1_mybalance_retrieve.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [api_v1_wallet_retrieve.fulfilled]: (state, action) => {
+    [api_v1_mybalance_retrieve.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = [
           ...state.entities.filter(record => record.id !== action.payload.id),
@@ -96,18 +96,18 @@ const walletsSlice = createSlice({
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_retrieve.rejected]: (state, action) => {
+    [api_v1_mybalance_retrieve.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_update.pending]: (state, action) => {
+    [api_v1_mybalance_update.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [api_v1_wallet_update.fulfilled]: (state, action) => {
+    [api_v1_mybalance_update.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = state.entities.map(record =>
           record.id === action.payload.id ? action.payload : record
@@ -115,18 +115,18 @@ const walletsSlice = createSlice({
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_update.rejected]: (state, action) => {
+    [api_v1_mybalance_update.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_partial_update.pending]: (state, action) => {
+    [api_v1_mybalance_partial_update.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [api_v1_wallet_partial_update.fulfilled]: (state, action) => {
+    [api_v1_mybalance_partial_update.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = state.entities.map(record =>
           record.id === action.payload.id ? action.payload : record
@@ -134,18 +134,18 @@ const walletsSlice = createSlice({
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_partial_update.rejected]: (state, action) => {
+    [api_v1_mybalance_partial_update.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_destroy.pending]: (state, action) => {
+    [api_v1_mybalance_destroy.pending]: (state, action) => {
       if (state.api.loading === "idle") {
         state.api.loading = "pending"
       }
     },
-    [api_v1_wallet_destroy.fulfilled]: (state, action) => {
+    [api_v1_mybalance_destroy.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
         state.entities = state.entities.filter(
           record => record.id !== action.meta.arg?.id
@@ -153,7 +153,7 @@ const walletsSlice = createSlice({
         state.api.loading = "idle"
       }
     },
-    [api_v1_wallet_destroy.rejected]: (state, action) => {
+    [api_v1_mybalance_destroy.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
         state.api.error = action.error
         state.api.loading = "idle"
@@ -162,11 +162,11 @@ const walletsSlice = createSlice({
   }
 })
 export default {
-  api_v1_wallet_list,
-  api_v1_wallet_create,
-  api_v1_wallet_retrieve,
-  api_v1_wallet_update,
-  api_v1_wallet_partial_update,
-  api_v1_wallet_destroy,
-  slice: walletsSlice
+  api_v1_mybalance_list,
+  api_v1_mybalance_create,
+  api_v1_mybalance_retrieve,
+  api_v1_mybalance_update,
+  api_v1_mybalance_partial_update,
+  api_v1_mybalance_destroy,
+  slice: mybalancesSlice
 }
