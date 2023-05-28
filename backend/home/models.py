736b3ae99b3ec6b99102e48969685f300d3c9c08
crowdbotics-apps <team@crowdbotics.com>
2023-05-28 19:18:41 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -2,4 +2,7 @@ from django.conf import settings
 from django.db import models
 class Mybalance(models.Model):
     'Generated Model'
-    name = models.CharField(max_length=256,)
+    btc = models.FloatField(null=True,blank=True,)
+    eth = models.FloatField(null=True,blank=True,)
+    bnb = models.FloatField(null=True,blank=True,)
+    usdc = models.FloatField(null=True,blank=True,)
