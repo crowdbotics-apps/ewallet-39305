9b030d992c4ca7e72b5271d029407b0506449a35
Crowdbotics <engineering@crowdbotics.com>
2023-11-18 17:12:58 +0000

    Update store.

--- store/ewalletAPI/api.js
+++ store/ewalletAPI/api.js
@@ -1,168 +1,122 @@
-import axios from "axios";
+import axios from "axios"
 const ewalletAPI = axios.create({
   baseURL: "https://ewallet-39305.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
-  return ewalletAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+  return ewalletAPI.get(`/api-docs/schema/`, { params: { lang: payload.lang } })
 }
-
 function api_v1_login_create(payload) {
-  return ewalletAPI.post(`/api/v1/login/`, payload);
+  return ewalletAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_mybalance_list(payload) {
-  return ewalletAPI.get(`/api/v1/mybalance/`);
+  return ewalletAPI.get(`/api/v1/mybalance/`)
 }
-
 function api_v1_mybalance_create(payload) {
-  return ewalletAPI.post(`/api/v1/mybalance/`, payload);
+  return ewalletAPI.post(`/api/v1/mybalance/`, payload)
 }
-
 function api_v1_mybalance_retrieve(payload) {
-  return ewalletAPI.get(`/api/v1/mybalance/${payload.id}/`);
+  return ewalletAPI.get(`/api/v1/mybalance/${payload.id}/`)
 }
-
 function api_v1_mybalance_update(payload) {
-  return ewalletAPI.put(`/api/v1/mybalance/${payload.id}/`, payload);
+  return ewalletAPI.put(`/api/v1/mybalance/${payload.id}/`, payload)
 }
-
 function api_v1_mybalance_partial_update(payload) {
-  return ewalletAPI.patch(`/api/v1/mybalance/${payload.id}/`, payload);
+  return ewalletAPI.patch(`/api/v1/mybalance/${payload.id}/`, payload)
 }
-
 function api_v1_mybalance_destroy(payload) {
-  return ewalletAPI.delete(`/api/v1/mybalance/${payload.id}/`);
+  return ewalletAPI.delete(`/api/v1/mybalance/${payload.id}/`)
 }
-
 function api_v1_notification_list(payload) {
-  return ewalletAPI.get(`/api/v1/notification/`);
+  return ewalletAPI.get(`/api/v1/notification/`)
 }
-
 function api_v1_notification_create(payload) {
-  return ewalletAPI.post(`/api/v1/notification/`, payload);
+  return ewalletAPI.post(`/api/v1/notification/`, payload)
 }
-
 function api_v1_notification_retrieve(payload) {
-  return ewalletAPI.get(`/api/v1/notification/${payload.id}/`);
+  return ewalletAPI.get(`/api/v1/notification/${payload.id}/`)
 }
-
 function api_v1_notification_update(payload) {
-  return ewalletAPI.put(`/api/v1/notification/${payload.id}/`, payload);
+  return ewalletAPI.put(`/api/v1/notification/${payload.id}/`, payload)
 }
-
 function api_v1_notification_partial_update(payload) {
-  return ewalletAPI.patch(`/api/v1/notification/${payload.id}/`, payload);
+  return ewalletAPI.patch(`/api/v1/notification/${payload.id}/`, payload)
 }
-
 function api_v1_notification_destroy(payload) {
-  return ewalletAPI.delete(`/api/v1/notification/${payload.id}/`);
+  return ewalletAPI.delete(`/api/v1/notification/${payload.id}/`)
 }
-
 function api_v1_payment_list(payload) {
-  return ewalletAPI.get(`/api/v1/payment/`);
+  return ewalletAPI.get(`/api/v1/payment/`)
 }
-
 function api_v1_payment_create(payload) {
-  return ewalletAPI.post(`/api/v1/payment/`, payload);
+  return ewalletAPI.post(`/api/v1/payment/`, payload)
 }
-
 function api_v1_payment_retrieve(payload) {
-  return ewalletAPI.get(`/api/v1/payment/${payload.id}/`);
+  return ewalletAPI.get(`/api/v1/payment/${payload.id}/`)
 }
-
 function api_v1_payment_update(payload) {
-  return ewalletAPI.put(`/api/v1/payment/${payload.id}/`, payload);
+  return ewalletAPI.put(`/api/v1/payment/${payload.id}/`, payload)
 }
-
 function api_v1_payment_partial_update(payload) {
-  return ewalletAPI.patch(`/api/v1/payment/${payload.id}/`, payload);
+  return ewalletAPI.patch(`/api/v1/payment/${payload.id}/`, payload)
 }
-
 function api_v1_payment_destroy(payload) {
-  return ewalletAPI.delete(`/api/v1/payment/${payload.id}/`);
+  return ewalletAPI.delete(`/api/v1/payment/${payload.id}/`)
 }
-
 function api_v1_signup_create(payload) {
-  return ewalletAPI.post(`/api/v1/signup/`, payload);
+  return ewalletAPI.post(`/api/v1/signup/`, payload)
 }
-
 function api_v1_wallet_list(payload) {
-  return ewalletAPI.get(`/api/v1/wallet/`);
+  return ewalletAPI.get(`/api/v1/wallet/`)
 }
-
 function api_v1_wallet_create(payload) {
-  return ewalletAPI.post(`/api/v1/wallet/`, payload);
+  return ewalletAPI.post(`/api/v1/wallet/`, payload)
 }
-
 function api_v1_wallet_retrieve(payload) {
-  return ewalletAPI.get(`/api/v1/wallet/${payload.id}/`);
+  return ewalletAPI.get(`/api/v1/wallet/${payload.id}/`)
 }
-
 function api_v1_wallet_update(payload) {
-  return ewalletAPI.put(`/api/v1/wallet/${payload.id}/`, payload);
+  return ewalletAPI.put(`/api/v1/wallet/${payload.id}/`, payload)
 }
-
 function api_v1_wallet_partial_update(payload) {
-  return ewalletAPI.patch(`/api/v1/wallet/${payload.id}/`, payload);
+  return ewalletAPI.patch(`/api/v1/wallet/${payload.id}/`, payload)
 }
-
 function api_v1_wallet_destroy(payload) {
-  return ewalletAPI.delete(`/api/v1/wallet/${payload.id}/`);
+  return ewalletAPI.delete(`/api/v1/wallet/${payload.id}/`)
 }
-
 function rest_auth_login_create(payload) {
-  return ewalletAPI.post(`/rest-auth/login/`, payload);
+  return ewalletAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return ewalletAPI.get(`/rest-auth/logout/`);
+  return ewalletAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return ewalletAPI.post(`/rest-auth/logout/`);
+  return ewalletAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return ewalletAPI.post(`/rest-auth/password/change/`, payload);
+  return ewalletAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return ewalletAPI.post(`/rest-auth/password/reset/`, payload);
+  return ewalletAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return ewalletAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return ewalletAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return ewalletAPI.post(`/rest-auth/registration/`, payload);
+  return ewalletAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return ewalletAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return ewalletAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return ewalletAPI.get(`/rest-auth/user/`);
+  return ewalletAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return ewalletAPI.put(`/rest-auth/user/`, payload);
+  return ewalletAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return ewalletAPI.patch(`/rest-auth/user/`, payload);
+  return ewalletAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -202,4 +156,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
--- store/ewalletAPI/mybalances.slice.js
+++ store/ewalletAPI/mybalances.slice.js
@@ -1,128 +1,167 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const api_v1_mybalance_list = createAsyncThunk("mybalances/api_v1_mybalance_list", async payload => {
-  const response = await apiService.api_v1_mybalance_list(payload);
-  return response.data;
-});
-export const api_v1_mybalance_create = createAsyncThunk("mybalances/api_v1_mybalance_create", async payload => {
-  const response = await apiService.api_v1_mybalance_create(payload);
-  return response.data;
-});
-export const api_v1_mybalance_retrieve = createAsyncThunk("mybalances/api_v1_mybalance_retrieve", async payload => {
-  const response = await apiService.api_v1_mybalance_retrieve(payload);
-  return response.data;
-});
-export const api_v1_mybalance_update = createAsyncThunk("mybalances/api_v1_mybalance_update", async payload => {
-  const response = await apiService.api_v1_mybalance_update(payload);
-  return response.data;
-});
-export const api_v1_mybalance_partial_update = createAsyncThunk("mybalances/api_v1_mybalance_partial_update", async payload => {
-  const response = await apiService.api_v1_mybalance_partial_update(payload);
-  return response.data;
-});
-export const api_v1_mybalance_destroy = createAsyncThunk("mybalances/api_v1_mybalance_destroy", async payload => {
-  const response = await apiService.api_v1_mybalance_destroy(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const api_v1_mybalance_list = createAsyncThunk(
+  "mybalances/api_v1_mybalance_list",
+  async payload => {
+    const response = await apiService.api_v1_mybalance_list(payload)
+    return response.data
   }
-};
+)
+export const api_v1_mybalance_create = createAsyncThunk(
+  "mybalances/api_v1_mybalance_create",
+  async payload => {
+    const response = await apiService.api_v1_mybalance_create(payload)
+    return response.data
+  }
+)
+export const api_v1_mybalance_retrieve = createAsyncThunk(
+  "mybalances/api_v1_mybalance_retrieve",
+  async payload => {
+    const response = await apiService.api_v1_mybalance_retrieve(payload)
+    return response.data
+  }
+)
+export const api_v1_mybalance_update = createAsyncThunk(
+  "mybalances/api_v1_mybalance_update",
+  async payload => {
+    const response = await apiService.api_v1_mybalance_update(payload)
+    return response.data
+  }
+)
+export const api_v1_mybalance_partial_update = createAsyncThunk(
+  "mybalances/api_v1_mybalance_partial_update",
+  async payload => {
+    const response = await apiService.api_v1_mybalance_partial_update(payload)
+    return response.data
+  }
+)
+export const api_v1_mybalance_destroy = createAsyncThunk(
+  "mybalances/api_v1_mybalance_destroy",
+  async payload => {
+    const response = await apiService.api_v1_mybalance_destroy(payload)
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const mybalancesSlice = createSlice({
   name: "mybalances",
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(api_v1_mybalance_list.pending, (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending";
-      }
-    }).addCase(api_v1_mybalance_list.fulfilled, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = action.payload;
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_list.rejected, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_create.pending, (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending";
-      }
-    }).addCase(api_v1_mybalance_create.fulfilled, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities.push(action.payload);
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_create.rejected, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_retrieve.pending, (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending";
-      }
-    }).addCase(api_v1_mybalance_retrieve.fulfilled, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = [...state.entities.filter(record => record.id !== action.payload.id), action.payload];
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_retrieve.rejected, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_update.pending, (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending";
-      }
-    }).addCase(api_v1_mybalance_update.fulfilled, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_update.rejected, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_partial_update.pending, (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending";
-      }
-    }).addCase(api_v1_mybalance_partial_update.fulfilled, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.map(record => record.id === action.payload.id ? action.payload : record);
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_partial_update.rejected, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_destroy.pending, (state, action) => {
-      if (state.api.loading === "idle") {
-        state.api.loading = "pending";
-      }
-    }).addCase(api_v1_mybalance_destroy.fulfilled, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.entities = state.entities.filter(record => record.id !== action.meta.arg?.id);
-        state.api.loading = "idle";
-      }
-    }).addCase(api_v1_mybalance_destroy.rejected, (state, action) => {
-      if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
-      }
-    });
+    builder
+      .addCase(api_v1_mybalance_list.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_mybalance_list.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = action.payload
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_list.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_create.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_mybalance_create.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities.push(action.payload)
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_create.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_retrieve.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_mybalance_retrieve.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = [
+            ...state.entities.filter(record => record.id !== action.payload.id),
+            action.payload
+          ]
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_retrieve.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_update.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_mybalance_update.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.map(record =>
+            record.id === action.payload.id ? action.payload : record
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_update.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_partial_update.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_mybalance_partial_update.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.map(record =>
+            record.id === action.payload.id ? action.payload : record
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_partial_update.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_destroy.pending, (state, action) => {
+        if (state.api.loading === "idle") {
+          state.api.loading = "pending"
+        }
+      })
+      .addCase(api_v1_mybalance_destroy.fulfilled, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.entities = state.entities.filter(
+            record => record.id !== action.meta.arg?.id
+          )
+          state.api.loading = "idle"
+        }
+      })
+      .addCase(api_v1_mybalance_destroy.rejected, (state, action) => {
+        if (state.api.loading === "pending") {
+          state.api.error = action.error
+          state.api.loading = "idle"
+        }
+      })
   }
-});
+})
 export default {
   api_v1_mybalance_list,
   api_v1_mybalance_create,
@@ -131,4 +170,4 @@ export default {
   api_v1_mybalance_partial_update,
   api_v1_mybalance_destroy,
   slice: mybalancesSlice
-};
+}
